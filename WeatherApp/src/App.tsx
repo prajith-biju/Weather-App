0fa024e7ef9e84f9393c58a00cfe941ebd4e4dbe
Prajith Biju <prajithbiju05@gmail.com>
2024-07-02 18:48:54 +0530

    first commit initial setups done

---
+++ WeatherApp/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h3 className="text-3xl font-bold text-red-400">something</h3>
+    </>
+  );
+}
+
+export default App;
